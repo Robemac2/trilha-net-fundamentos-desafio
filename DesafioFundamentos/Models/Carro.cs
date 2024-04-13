12c1a8d3ddcc3eb94790e8901db4b5a20eea3cee
Roberto Macedo <robertoneto5@hotmail.com>
2024-04-12 21:09:55 -0300

    Implementado a classe Carro

--- DesafioFundamentos/Models/Carro.cs
+++ DesafioFundamentos/Models/Carro.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,24 @@ namespace DesafioFundamentos.Models
 {
     internal class Carro
     {
+        private readonly string modelo;
+        private readonly string cor;
+        private readonly string placa;
 
+        public Carro(string modelo, string cor, string placa)
+        {
+            this.modelo = modelo;
+            this.cor = cor;
+            this.placa = placa;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder st = new StringBuilder();
+            st.Append("Modelo: " + this.modelo);
+            st.Append(" Cor: " + this.cor);
+            st.Append(" Placa: " + this.placa);
+            return st.ToString();
+        }
     }
 }
