b79365a3222dd43a323ff5b41200aa606c9f30cc
Roberto Macedo <robertoneto5@hotmail.com>
2025-05-17 13:09:02 -0300

    Criado classe Carro.cs

---
+++ DesafioFundamentos/Models/Carro.cs
@@ -0,0 +1,22 @@
+﻿namespace DesafioFundamentos.Models;
+
+public class Carro
+{
+    private string _placa;
+    private string _modelo;
+    private string _cor;
+    private string _marca;
+
+    public string Placa => _placa;
+    public string Modelo => _modelo;
+    public string Cor => _cor;
+    public string Marca => _marca;
+
+    public Carro(string marca, string modelo, string cor, string placa)
+    {
+        this._placa = placa;
+        this._modelo = modelo;
+        this._cor = cor;
+        this._marca = marca;
+    }
+}
